6ecc66c43220ce5bba11a044358102871e113bf3
Gerhard Heide <gerhard.heide@web.de>
2026-01-17 16:11:55 +0100

    remerge

--- TrayApplicationContext.cs
+++ TrayApplicationContext.cs
@@ -71,6 +71,13 @@ namespace FlyMenu
             flyoutMenu = new ContextMenuStrip();
             flyoutMenu.Closed += (s, e) => { /* no-op - poller controls show/hide */ };
 
+            // Create the app menu container (items will be populated on demand)
+            appMenu = new ContextMenuStrip();
+            appMenu.Closed += (s, e) => { /* no-op - poller controls show/hide */ };
+
+            // Set menu references in MenuActionHandler so actions can close menus
+            MenuActionHandler.SetMenuReferences(flyoutMenu, appMenu);
+
             // Subscribe to VirtualDesktop changes
             System.Diagnostics.Debug.WriteLine("TrayApplicationContext: Subscribing to VirtualDesktop.CurrentChanged...");
             VirtualDesktop.CurrentChanged += OnVirtualDesktopCurrentChanged;
